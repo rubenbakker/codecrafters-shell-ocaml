f979f72fe733bb50c7fa86867ce629f5095810ee
Ruben Bakker <ruben@uncomplex.ch>
2026-02-22 21:56:35 +0100

    codecrafters submit [skip ci]

--- lib/builtins.ml
+++ lib/builtins.ml
@@ -5,7 +5,8 @@ let echo args = Lwt_io.printl (String.concat ~sep:" " args)
 
 let type_ arg =
   match arg with
-  | "exit" | "echo" | "type" -> Lwt_io.printlf "%s is a shell builtin" arg
+  | "exit" | "echo" | "type" | "pwd" ->
+      Lwt_io.printlf "%s is a shell builtin" arg
   | _ -> (
       match Executable.full_path arg with
       | Some path -> Lwt_io.printlf "%s is %s" arg path
