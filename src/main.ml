fad93419e7c58882824b0a5588b6726d226e021e
Ruben Bakker <ruben@uncomplex.ch>
2026-02-22 13:33:13 +0100

    codecrafters submit [skip ci]

--- src/main.ml
+++ src/main.ml
@@ -3,9 +3,12 @@ open! Base
 let ( let* ) = Lwt.bind
 
 let repl () =
+  let rec goo () =
     let* () = Lwt_io.print "$ " in
     let* line = Lwt_io.read_line Lwt_io.stdin in
-    Lwt_io.printlf "%s: command not found" line
+    let* () = Lwt_io.printlf "%s: command not found" line in
+    goo () in
+  goo ()
 
 let () =
   Lwt_main.run (repl ())
