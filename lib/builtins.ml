90c4f5e593b80084c405aa5b388fe026715f5842
Ruben Bakker <ruben@uncomplex.ch>
2026-02-22 21:39:00 +0100

    codecrafters submit [skip ci]

--- lib/builtins.ml
+++ lib/builtins.ml
@@ -3,11 +3,19 @@ open! Base
 let exit () = Unix._exit 0
 let echo args = Lwt_io.printl (String.concat ~sep:" " args)
 
+let is_executable fullpath =
+  try
+    Unix.access fullpath [ Unix.X_OK ];
+    true
+  with Unix.Unix_error _ -> false
+
 let path_for_executable executable_name =
   let path = Unix.getenv "PATH" |> String.split ~on:':' in
   List.map path ~f:(fun p ->
-      let filename = Stdlib.Filename.concat p executable_name in
-      if Stdlib.Sys.file_exists filename then Some filename else None)
+      let fullpath = Stdlib.Filename.concat p executable_name in
+      if Stdlib.Sys.file_exists fullpath && is_executable fullpath then
+        Some fullpath
+      else None)
   |> List.filter_opt |> List.hd
 
 let type_ arg =
