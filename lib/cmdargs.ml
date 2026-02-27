d222138f8c36a4fe0fdb02d528ec4cb5b7cafb81
Ruben Bakker <ruben@uncomplex.ch>
2026-02-27 16:52:18 +0100

    codecrafters submit [skip ci]

--- lib/cmdargs.ml
+++ lib/cmdargs.ml
@@ -3,6 +3,7 @@ open! Base
 type scanner_state_t = Normal | SingleQuote | DoubleQuote
 
 let rec scan state chars acc args =
+  let is_whitespace_char char = Char.(char = ' ' || char = '\t') in
   let add_arg acc args =
     if not (List.is_empty acc) then
       (List.rev acc |> String.of_char_list) :: args
@@ -13,6 +14,9 @@ let rec scan state chars acc args =
   | SingleQuote, '\'' :: rest -> scan Normal rest [] (add_arg acc args)
   | SingleQuote, char :: rest -> scan SingleQuote rest (char :: acc) args
   | DoubleQuote, '"' :: '"' :: rest -> scan DoubleQuote rest acc args
+  | DoubleQuote, '"' :: char :: rest
+    when is_whitespace_char char && List.length acc > 0 ->
+      scan Normal rest [] (add_arg acc args)
   | DoubleQuote, '"' :: rest -> scan Normal rest [] (add_arg acc args)
   | DoubleQuote, '\\' :: char :: rest
     when Char.(
@@ -25,8 +29,7 @@ let rec scan state chars acc args =
   | Normal, '"' :: '"' :: rest -> scan Normal rest acc args
   | Normal, '"' :: rest -> scan DoubleQuote rest [] args
   | Normal, '\\' :: char :: rest -> scan Normal rest (char :: acc) args
-  | Normal, char :: rest
-    when Char.(char = ' ' || char = '\t') && List.length acc > 0 ->
+  | Normal, char :: rest when is_whitespace_char char && List.length acc > 0 ->
       scan Normal rest [] (add_arg acc args)
   | Normal, char :: rest when Char.(char = ' ' || char = '\t') ->
       scan Normal rest acc args
