0aa9c94fbdd3585567ba6f68f358b10458d4e7db
Ruben Bakker <ruben@uncomplex.ch>
2026-02-28 22:00:50 +0100

    codecrafters submit [skip ci]

--- lib/cmdargs.ml
+++ lib/cmdargs.ml
@@ -39,6 +39,8 @@ let rec scan state chars acc args =
       scan Normal rest [] ("1>>" :: add_arg acc args)
   | Normal, '>' :: rest | Normal, '1' :: '>' :: rest ->
       scan Normal rest [] ("1>" :: add_arg acc args)
+  | Normal, '2' :: '>' :: '>' :: rest ->
+      scan Normal rest [] ("2>>" :: add_arg acc args)
   | Normal, '2' :: '>' :: rest -> scan Normal rest [] ("2>" :: add_arg acc args)
   | Normal, '\'' :: '\'' :: rest -> scan Normal rest acc args
   | Normal, '\'' :: rest -> scan SingleQuote rest acc args
