56a78a8c748b8aa2fbbf2e211c005d1abe5757e5
Ruben Bakker <ruben@uncomplex.ch>
2026-03-04 10:18:32 +0100

    codecrafters submit [skip ci]

--- lib/repl.ml
+++ lib/repl.ml
@@ -2,38 +2,28 @@ open! Base
 open Lwt.Infix
 open Lwt.Let_syntax
 
-let user_input prompt =
-  match LNoise.linenoise prompt with
-  | Some result -> Lwt.return result
-  | None -> Lwt.return ""
-;;
+let completion str = Readline.Custom [ "exit", ' '; "echo", ' ' ]
 
-let setup_completion () =
-  LNoise.set_completion_callback (fun line_so_far ln_completions ->
-    if String.is_empty line_so_far
-    then ()
-    else
-      List.filter Builtins.all ~f:(fun builtin ->
-        String.is_prefix ~prefix:line_so_far builtin)
-      |> List.map ~f:(fun completion -> String.append completion " ")
-      |> List.iter ~f:(LNoise.add_completion ln_completions));
-  ()
+let user_input prompt =
+  Lwt.return (Readline.readline ~prompt:"$ " ~completion_fun:completion ())
 ;;
 
 let repl () =
-  setup_completion ();
+  Readline.init ();
   let rec go () =
     let%bind _ = Lwt_io.flush_all () in
-    let%bind line = user_input "$ " in
-    let args = Cmdargs.parse line in
-    match args.args with
-    | "exit" :: [] -> Builtins.exit ()
-    | "echo" :: _rest -> Builtins.echo args >>= go
-    | [ "type"; arg ] -> Builtins.type_ arg >>= go
-    | [ "pwd" ] -> Builtins.pwd () >>= go
-    | [ "cd"; path ] -> Builtins.cd path >>= go
-    | _command :: _rest -> Executable.exec args >>= Lwt_io.flush_all >>= go
-    | _ -> Lwt_io.printlf "%s: command not found" line >>= go
+    match%bind user_input "$ " with
+    | None -> Lwt.return_unit
+    | Some line ->
+      let args = Cmdargs.parse line in
+      (match args.args with
+       | "exit" :: [] -> Builtins.exit ()
+       | "echo" :: _rest -> Builtins.echo args >>= go
+       | [ "type"; arg ] -> Builtins.type_ arg >>= go
+       | [ "pwd" ] -> Builtins.pwd () >>= go
+       | [ "cd"; path ] -> Builtins.cd path >>= go
+       | _command :: _rest -> Executable.exec args >>= Lwt_io.flush_all >>= go
+       | _ -> Lwt_io.printlf "%s: command not found" line >>= go)
   in
   go ()
 ;;
